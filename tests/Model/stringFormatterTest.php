505dc9a689009b8d908fdec7d0dd397ca2b2f0a1
berangere.la.touche <berangere.lt@gmail.com>
2017-07-05 11:47:41 +0200

    test concat done

--- tests/Model/stringFormatterTest.php
+++ tests/Model/stringFormatterTest.php
@@ -6,7 +6,13 @@ use Model\StringFormatter
 
 class StringFormatterTest extends \PHPUnit_Framework_TestCase {
 
-  
+  public function test_Concat() {
+      $string1 = 'Bonjour';
+      $string2 = 'Aurevoir';
+
+      $result = StringFormater::concat($string1, $string2);
+      $this->assertSame('BonjourAurevoir', $result);
+  }
 
 }
  ?>
